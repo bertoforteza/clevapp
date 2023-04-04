30c8d0908126e4cdb391f0a052395bdc484e8dc4
Berto Forteza <bertoforteza87@gmail.com>
2023-04-04 19:20:37 +0200

    Add GlobalStyle and ThemeProvider to main index file

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import "@fontsource/work-sans";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import { ThemeProvider } from "styled-components";
 import { store } from "./store/store";
+import mainTheme from "./styles/mainTheme";
+import GlobalStyle from "./styles/GlobalStyle";
 import App from "./components/App/App";
-import "./index.css";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
@@ -11,7 +14,10 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={mainTheme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );

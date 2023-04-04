781decf950df8a1eb14d0ea313f22e8b5b351f3e
Berto Forteza <bertoforteza87@gmail.com>
2023-04-04 19:14:10 +0200

    Create GlobalStyle file

---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+
+*, 
+::before,
+::after {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Work Sans", sans-serif;
+}
+
+body {
+  background-color: ${(props) => props.theme.colors.ink.light}
+}
+
+body, h1, h2, ul, li, p, span {
+  margin: 0;
+}
+
+ol, ul {
+  list-style: none;
+  padding: 0;
+}
+`;
+
+export default GlobalStyle;

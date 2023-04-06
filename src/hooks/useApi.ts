1cc18ad6d898bbc6e26292673834fd3f336c9f58
Berto Forteza <bertoforteza87@gmail.com>
2023-04-06 09:58:24 +0200

    Fix getPosts function adding useCallback

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -5,19 +5,20 @@ import {
   deletePostActionCreator,
   loadPostsActionCreator,
 } from "../store/features/postsSlice";
+import { useCallback } from "react";
 
 const apiUrl = process.env.REACT_APP_API_URL!;
 
 const useApi = () => {
   const dispatch = useAppDispatch();
 
-  const getPosts = async () => {
+  const getPosts = useCallback(async () => {
     const response = await axios.get(apiUrl);
 
     const posts: ApiResponse = response.data;
 
     dispatch(loadPostsActionCreator(posts));
-  };
+  }, [dispatch]);
 
   const deletePost = (id: number) => {
     dispatch(deletePostActionCreator(id));

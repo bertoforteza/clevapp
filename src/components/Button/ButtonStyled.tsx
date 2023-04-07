f80fa6b9315d1e2c230d277b2498ee21a250691c
Berto Forteza <bertoforteza87@gmail.com>
2023-04-07 18:34:38 +0200

    Refactor Button component to add disabled property

--- src/components/Button/ButtonStyled.tsx
+++ src/components/Button/ButtonStyled.tsx
@@ -4,14 +4,21 @@ const ButtonStyled = styled.button`
   background-color: ${(props) => props.theme.colors.secondary.base};
   color: ${(props) => props.theme.colors.primary.base};
   border-radius: 15px;
+  border: 2px solid transparent;
   font-size: 1rem;
   font-weight: 700;
   padding: 0.3rem 0.5rem;
-  border: none;
   cursor: pointer;
 
   &:hover {
     background-color: ${(props) => props.theme.colors.secondary.light};
+  }
+
+  &:disabled {
+    background-color: transparent;
+    border: 2px solid ${(props) => props.theme.colors.primary.light};
+    color: ${(props) => props.theme.colors.primary.light};
+    cursor: auto;
   }
 `;
 

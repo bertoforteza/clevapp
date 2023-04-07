ccc32180869fa780f97af23ef98cc42df6880a5b
Berto Forteza <bertoforteza87@gmail.com>
2023-04-07 20:27:59 +0200

    Add Layout component to App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,20 +1,7 @@
-import HomePage from "../../pages/HomePage/HomePage";
-import { useAppSelector } from "../../store/hooks";
-import Header from "../Header/Header";
-import Modal from "../Modal/Modal";
+import Layout from "../Layout/Layout";
 
-const App = () => {
-  const {
-    modal: { isError, isOpen, message },
-  } = useAppSelector(({ ui }) => ui);
-
-  return (
-    <div className="container">
-      {isOpen && <Modal isError={isError} message={message} />}
-      <Header />
-      <HomePage />
-    </div>
-  );
+const App = (): JSX.Element => {
+  return <Layout />;
 };
 
 export default App;

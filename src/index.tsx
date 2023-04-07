7ecd8cf3eb82ad4cddf468f3f6e08b3cdbc2c398
Berto Forteza <bertoforteza87@gmail.com>
2023-04-07 19:52:59 +0200

    Add RouterProvider to main index

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,12 @@ import "@fontsource/work-sans";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { ThemeProvider } from "styled-components";
+import { RouterProvider } from "react-router-dom";
 import { store } from "./store/store";
 import mainTheme from "./styles/mainTheme";
 import GlobalStyle from "./styles/GlobalStyle";
 import App from "./components/App/App";
+import router from "./router/router";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
@@ -16,6 +18,7 @@ root.render(
     <Provider store={store}>
       <ThemeProvider theme={mainTheme}>
         <GlobalStyle />
+        <RouterProvider router={router} />
         <App />
       </ThemeProvider>
     </Provider>

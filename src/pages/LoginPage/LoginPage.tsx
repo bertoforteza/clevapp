126dc63046553618caa5bbb4ea8a593b6eec4d0d
Berto Forteza <bertoforteza87@gmail.com>
2023-04-08 15:00:54 +0200

    Add navigation to HomePage in LoginPage when isLogged is true

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,8 +1,13 @@
+import { Navigate } from "react-router-dom";
 import LoginForm from "../../components/LoginForm/LoginForm";
+import { useAppSelector } from "../../store/hooks";
 import LoginPageStyled from "./LoginPageStyled";
 
 const LoginPage = (): JSX.Element => {
-  return (
+  const { isLogged } = useAppSelector(({ user }) => user);
+  return isLogged ? (
+    <Navigate to={"/"} />
+  ) : (
     <LoginPageStyled>
       <h2 className="login-title">login</h2>
       <LoginForm />

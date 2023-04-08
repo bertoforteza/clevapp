3518ddf5a42d379a5306857bbf9070a68531a136
Berto Forteza <bertoforteza87@gmail.com>
2023-04-08 19:24:06 +0200

    Add getUserToken function to App with useEffect

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
+import useUser from "../../hooks/useUser/useUser";
 import Layout from "../Layout/Layout";
 
 const App = (): JSX.Element => {
+  const { getUserToken } = useUser();
+
+  useEffect(() => {
+    getUserToken();
+  }, [getUserToken]);
+
   return <Layout />;
 };
 

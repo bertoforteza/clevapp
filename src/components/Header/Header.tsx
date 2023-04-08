cfd679a63541e800f02f6e07d33f0046441adf26
Berto Forteza <bertoforteza87@gmail.com>
2023-04-08 18:45:50 +0200

    Add logout button to Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,21 @@
 import { ReactComponent as Logo } from "../../assets/cleverpy-logo.svg";
+import { ReactComponent as Logout } from "../../assets/logout-icon.svg";
+import useUser from "../../hooks/useUser/useUser";
+import { useAppSelector } from "../../store/hooks";
+import Button from "../Button/Button";
 import HeaderStyled from "./HeaderStyled";
 
 const Header = () => {
+  const { logoutUser } = useUser();
+  const { isLogged } = useAppSelector(({ user }) => user);
+
   return (
     <HeaderStyled className="header">
-      <Logo />
-      <h1 className="header__title">Clevapp</h1>
+      <div className="header-container">
+        <Logo />
+        <h1 className="header__title">Clevapp</h1>
+      </div>
+      {isLogged && <Button icon={<Logout />} action={logoutUser} />}
     </HeaderStyled>
   );
 };
--- src/components/Header/HeaderStyled.tsx
+++ src/components/Header/HeaderStyled.tsx
@@ -4,15 +4,32 @@ const HeaderStyled = styled.header`
   display: flex;
   gap: 1.5rem;
   align-items: center;
+  justify-content: space-between;
   padding: 1rem 2rem;
   background-color: ${(props) => props.theme.colors.secondary.base};
   border-bottom: 5px solid ${(props) => props.theme.colors.primary.base};
 
   .header {
+    &-container {
+      display: flex;
+      gap: 1.5rem;
+      align-items: center;
+    }
+
     &__title {
       color: ${(props) => props.theme.colors.primary.base};
       font-size: 2.5rem;
       font-weight: 700;
+    }
+  }
+
+  .button {
+    display: flex;
+    padding: 0;
+    border: none;
+
+    &:hover {
+      background-color: ${(props) => props.theme.colors.secondary.base};
     }
   }
 `;

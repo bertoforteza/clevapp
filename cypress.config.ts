17df7ba640a1350b28bb3d95a85bb4caf5c80926
Berto Forteza <bertoforteza87@gmail.com>
2023-04-09 21:49:10 +0200

    Create login test with cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from "cypress";
 
+const url = "http://localhost:3000";
+
 export default defineConfig({
+  env: {
+    apiUrl: "https://jsonplaceholder.typicode.com/posts",
+  },
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    setupNodeEvents(on, config) {},
+    baseUrl: url,
   },
 });
